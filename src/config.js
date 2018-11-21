68360a08d4f0475337aa6e2a372709aedf63c0f6
imhcg <imhcg@live.com>
2018-11-21 21:22:06 +0800

    改正错别字

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   // 代理网关，接受以POST请求发送的含有url字段的Json数据，并返回该url的请求结果 
   PROX_GATE:'https://web.imhcg.cn/php/proxy/',
-  // 追书神奇的API，详细说明请参考 API.md
+  // 追书神器的API，详细说明请参考 API.md
   API:{
     Statics:"https://statics.zhuishushenqi.com",
     Search:"https://api.zhuishushenqi.com/book/fuzzy-search?query=",
